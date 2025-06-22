6390b45b708d1374bb0b8e4bff2208d1e1472e5c
mikemuturi <mikeirungumuturi@gmail.com>
2025-06-22 19:36:31 +0300

    2. Set up Header, Footer, Layout and landing page

--- alx-movie-app/components/commons/Button.tsx
+++ alx-movie-app/components/commons/Button.tsx
@@ -1,5 +1,14 @@
-const Button = () => {
-  return <button className="bg-blue-500 text-white px-4 py-2 rounded">Click Me</button>;
+import { ButtonProps } from "@/interfaces";
+
+const Button: React.FC<ButtonProps> = ({ title, action }) => {
+  return (
+    <button
+      onClick={action}
+      className="px-8 py-2 border-2 border-[#E2D609] rounded-full hover:bg-[#E2D609] hover:text-black transition-colors duration-300"
+    >
+      {title}
+    </button>
+  );
 };
 
 export default Button;
